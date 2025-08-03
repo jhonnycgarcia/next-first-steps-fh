25083c74055ca0d877b81525d67dafb3813f78a3
Jhonny <jhonnycgarcia@gmail.com>
2025-08-02 19:49:03 -0500

    resumen de la seccion

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center p-24">
       <span className="text-5xl">
         Hola Mundo
-        { new Date().getTime() }
       </span>
+      <Link href={'/about'}>About Page</Link>
     </main>
   );
 }
